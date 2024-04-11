2c2255a97291d51ffdacaec88fbdd8e9e242417d
Jack Hogg <159823673+JarkLark@users.noreply.github.com>
2024-04-11 13:38:52 -0400

    Add comments

---
+++ javaVariables/src/JavaVar.java
@@ -0,0 +1,17 @@
+
+public class JavaVar {
+//datatypes
+//class level vars
+    public static void main(String[] args) throws Exception {
+        // datatype varName [=value];
+        int games = 3;
+        double allowance = 40.50;
+        long battlepass; //we define battlepass
+        battlepass = 15; //we assign a value to battle pass
+
+        // print out our var values along with a string that explains our value
+        System.out.println("Number of games I own = " + games);
+        System.out.println("My monthly allowance = " + allowance);
+        System.out.println("Cost of a single battle pass = " + battlepass);
+    }
+}
